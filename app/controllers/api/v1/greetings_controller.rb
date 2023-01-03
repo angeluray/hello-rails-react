b573175ad35d178e7ad572f664024b2ab8af18f9
angeluray <aeucampos@gmail.com>
2023-01-02 21:25:17 -0400

    Update dependencies

--- app/controllers/api/v1/greetings_controller.rb
+++ app/controllers/api/v1/greetings_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::GreetingsController < ApplicationController
   def index
-    greetings = Greeting.all
-    render json: greetings, status: 200
+    @greetings = Greeting.all.sample
+    render json: @greetings, status: 200
   end
 end
